2d98feef65f0b5a22c035e2b570adc8dc495dea2
Pushpraj Patel <patelpushpraj35@gmail.com>
2026-04-25 14:28:30 +0530

    Nuclear CORS fix

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,12 +7,10 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://collabrix-be036.web.app');
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-  if (req.method === 'OPTIONS') {
-    return res.sendStatus(200);
-  }
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (req.method === 'OPTIONS') return res.sendStatus(200);
   next();
 });
 app.use(express.json());
