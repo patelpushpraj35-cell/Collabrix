fec4aa3936a1daafeb63157c30aa1a357e07ad6c
Pushpraj Patel <patelpushpraj35@gmail.com>
2026-04-25 14:11:21 +0530

    Switch to Firebase Functions

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,22 @@
 const { onRequest } = require("firebase-functions/v2/https");
 const { setGlobalOptions } = require("firebase-functions/v2");
-const app = require("./src/server");
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+app.use(cors({ origin: true }));
+app.use(express.json());
+
+// Routes
+app.use('/auth', require('./src/routes/auth'));
+app.use('/admin', require('./src/routes/admin'));
+app.use('/creators', require('./src/routes/creators'));
+app.use('/brands', require('./src/routes/brands'));
+
+app.get('/health', (req, res) => {
+  res.json({ status: 'ok', timestamp: new Date().toISOString() });
+});
 
 setGlobalOptions({ region: "us-central1" });
 
